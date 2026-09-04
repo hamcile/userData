c36170e36cee9f184d782ac998527f89a72e3077
hamcile <hamcilemich@gmail.com>
2026-09-04 12:19:42 +0100

    set up my venv and create a simple obj

---
+++ userdata/nameobj.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, name: str, age: int):
+        """this intialize out user's data for this project"""
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return f"Hello! my name is {self.name} and I am {self.age} years old. what about you?"
+
+u1: User = User("John", 16)
+
+print(u1.greet())
