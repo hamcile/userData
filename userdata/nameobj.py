98ca8d8c610f6f843bbfc9e295ef51edd3a8fe04
hamcile <hamcilemich@gmail.com>
2026-09-09 12:15:48 +0100

    created an app that takes user entry and stores in memory

--- userdata/nameobj.py
+++ userdata/nameobj.py
@@ -8,6 +8,79 @@ class User:
         """returns a greeting with the user name and age when ever it is called."""
         return f"Hello! my name is {self.name} and I am {self.age} years old. what about you?"
 
-u1: User = User("John", 16)
+userData: list = []
 
-print(u1.greet())
+def getUserData() -> list:
+    """this function takes in user inputs, cleans it up and stores in a list and returns a list of user datails."""
+    name = ""
+    age = 0
+
+    try:
+        name = input("Enter Your Name: ").strip().lower()
+    except ValueError:
+        print("Enter a Valid Name.")
+
+    try: 
+        age = int(input("Enter Your Age: "))
+    except ValueError:
+        print("Enter a Valid Number.")
+
+    thisUser: User = User(name, age)
+    userData.append(thisUser)
+    return userData
+    
+
+# getUserData()
+# for user in userData:
+#     print (user.name)
+
+
+def checkUserStatus():
+    """this function check if the user is a new user or not. if he is, we would call the getUserdata function and if he is an already exiting user, we tell him his details are already stored"""
+    print("ARE YOU A NEW USER?")
+    newUser: bool = True
+    while newUser:
+        
+        checkSatus: str = input("YES OR NO: ").lower().strip()
+        if checkSatus == "yes":
+            print ("LETS GET YOUR REGISTERED.")
+            getUserData()
+            print("Your details have been saved successfully.\n")
+            print("ARE YOU A NEW USER?")
+        elif checkSatus == "no":
+            print("You Are a Member Already, Glad To See You Again.")
+            break
+        else:
+            print("Choose Either Yes or No.")
+    
+        
+
+
+
+checkUserStatus()
+print(len(userData))
+for user in userData:
+    print (user.greet())
+
+# '''while newUser:
+#     print("ARE YOU A NEW USER?")
+#     checkSatus: str = input("YES OR NO: ").lower().strip()
+#     if checkSatus == "yes":
+#         newUser = True
+#         print ("LETS GET YOUR REGISTERED.")
+#         userName: str = input("full name please: ").lower().strip()
+#         age: int = int(input("what is your age: "))
+        
+#     elif checkSatus == "no":
+#         newUser = False
+#         print("YOUR DATA IS SAFE WITH US.")
+#     elif checkSatus != "no" or checkSatus != "yes":
+#         print("SELECT A VALID OPTION.")
+
+
+#     person: User = User(userName, age)
+#     userData.append(person)
+
+# if newUser == False:
+#     print(len(userData))
+#     print(userData)'''
